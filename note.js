023c8b5209e60c83a4c49fe5446537f8f29e1175
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-05-21 18:07:19 +0800

    first commit

---
+++ note.js
@@ -0,0 +1,8 @@
+const gameData = [
+    { id: 1, image:'/img/yasai.png', word:'やさい'  },
+    { id: 2, image:'/img/tamago.png', word:'たまご'  },
+    { id: 3, image:'/img/sakana.png', word:'さかな'  },
+    { id: 4, image:'/img/niku.png', word:'にく' },
+    { id: 5, image:'/img/gohan.png', word:'ごはん' }
+  
+];
